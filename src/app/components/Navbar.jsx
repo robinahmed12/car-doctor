b6267f2a4a05d9b7d308046de00266c31aa413b8
robinahmed12 <shawonahmed0294@gmail.com>
2025-08-11 16:50:46 +0600

    initialize next js set up

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from './ui/Container';
+
+const Navbar = () => {
+    return (
+        <Container>
+            <div>
+            <h1>hi</h1>
+        </div>
+        </Container>
+    );
+};
+
+export default Navbar;
